594a8bddd4799d9c8bd6fdddacce59475cf6d124
Philip Pretorius <philippretorius.79@gmail.com>
2023-04-25 15:20:42 +0200

    Toggle Display progress

--- js.js
+++ js.js
@@ -3,8 +3,10 @@
 
 const gameBoard = (() => {
 
+  const items = [];
+
   function createGameBoard() {
-    const items = [];
+    
     const body = document.getElementById('body');
 
     for (let i = 0; i < 9; i++) {
@@ -15,17 +17,46 @@ const gameBoard = (() => {
 
       items[i].classList.add(className);
       items[i].textContent = number;
+      items[i].addEventListener('click', toggleDisplay);
       body.append(items[i]);
     };
   };
 
   return {
-    createGameBoard: createGameBoard
+    createGameBoard: createGameBoard,
+    gameBoardArray: items
   };
 
 })();
 
 gameBoard.createGameBoard();
+console.log(gameBoard.gameBoardArray);
+
+// Factory Function to create player info
+const player = (name, sign) => {
+  
+  const getName = () => name;
+  const getSign = () => sign;
+
+  return {
+    getName,
+    getSign
+  };
+
+};
+
+const player1 = player('P1', 'X'); // change that player inputs it in Phase 1
+const player2 = player('P2', 'O'); // change that player inputs it in Phase 1
+
+//// Experimental Area
+
+function toggleDisplay() {
+/*   if (player.getSign === 'X') { */
+    this.innerText = 'X';
+/*   } */
+}
 
 
-
+(onclick) => {
+  this.innerText = 'X';
+}
